f2adb972c18507ec6730d3f9969209ac9bc2dab8
bartwe2 <barwer2@gmail.com>
2018-10-17 12:36:07 +0200

    deploy fix

--- src/main/java/server/StartApp.java
+++ src/main/java/server/StartApp.java
@@ -7,6 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -15,7 +16,7 @@ import java.util.Date;
 @EntityScan(basePackages = {"domain"})
 @EnableJpaRepositories(basePackages = {"domain.repos"})
 @SpringBootApplication
-public class StartApp {
+public class StartApp extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(StartApp.class, args);
