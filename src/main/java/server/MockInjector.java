336f4bce3c8ab8dd62c35a6be8a184c1990a9e98
ganymede95 <195060@edu.p.lodz.pl>
2018-11-06 20:21:49 +0100

    Added more mock events and matches
    Note: Latitudes and longitudes are taken from actual pubs (notice the extremely small differences between them). This data can be used in the future for pub in-radius searching.

--- src/main/java/server/MockInjector.java
+++ src/main/java/server/MockInjector.java
@@ -21,28 +21,94 @@ public class MockInjector {
             eventRepository.save(Event.builder()
                     .matchId(1L)
                     .pubId(1L)
-                    .latitude(23.22)
-                    .longitude(44.22)
+                    .latitude(51.7519858)
+                    .longitude(19.452498)
                     .numberOfAttendees(20L)
-                    .description("dużo piwa")
+                    .description("Real Madryt VS Liverpool pub 1 INDEKS")
                     .build());
 
             eventRepository.save(Event.builder()
-                    .matchId(2L)
+                    .matchId(1L)
                     .pubId(2L)
-                    .latitude(55.21)
-                    .longitude(77.22)
-                    .numberOfAttendees(48L)
-                    .description("free koks")
+                    .latitude(51.7465619)
+                    .longitude(19.4502251)
+                    .numberOfAttendees(20L)
+                    .description("Real Madryt VS Liverpool pub 2 COTTON CLUB")
                     .build());
 
             eventRepository.save(Event.builder()
                     .matchId(1L)
                     .pubId(3L)
-                    .latitude(22.21)
-                    .longitude(17.22)
-                    .numberOfAttendees(10L)
-                    .description("pizzza day")
+                    .latitude(51.768345)
+                    .longitude(19.459083)
+                    .numberOfAttendees(20L)
+                    .description("Real Madryt VS Liverpool pub 3 Z INNEJ BECZKI")
+                    .build());
+
+            eventRepository.save(Event.builder()
+                    .matchId(1L)
+                    .pubId(4L)
+                    .latitude(51.7664589)
+                    .longitude(19.4575109)
+                    .numberOfAttendees(20L)
+                    .description("Real Madryt VS Liverpool pub 4 LONG PLAY")
+                    .build());
+
+
+            eventRepository.save(Event.builder()
+                    .matchId(1L)
+                    .pubId(2L)
+                    .latitude(51.7465619)
+                    .longitude(19.4502251)
+                    .numberOfAttendees(20L)
+                    .description("Tottenham VS Schalke pub 2 COTTON CLUB")
+                    .build());
+
+            eventRepository.save(Event.builder()
+                    .matchId(1L)
+                    .pubId(3L)
+                    .latitude(51.768345)
+                    .longitude(19.459083)
+                    .numberOfAttendees(20L)
+                    .description("Tottenham VS Schalke pub 3 Z INNEJ BECZKI")
+                    .build());
+
+            eventRepository.save(Event.builder()
+                    .matchId(1L)
+                    .pubId(4L)
+                    .latitude(51.7664589)
+                    .longitude(19.4575109)
+                    .numberOfAttendees(20L)
+                    .description("Tottenham VS Schalke pub 4 LONG PLAY")
+                    .build());
+
+
+            eventRepository.save(Event.builder()
+                    .matchId(1L)
+                    .pubId(1L)
+                    .latitude(51.7465619)
+                    .longitude(19.4502251)
+                    .numberOfAttendees(20L)
+                    .description("Liverpool vs RM pub 2 COTTON CLUB")
+                    .build());
+
+            eventRepository.save(Event.builder()
+                    .matchId(1L)
+                    .pubId(4L)
+                    .latitude(51.7664589)
+                    .longitude(19.4575109)
+                    .numberOfAttendees(20L)
+                    .description("Liverpool vs RM pub 4 LONG PLAY")
+                    .build());
+
+
+            eventRepository.save(Event.builder()
+                    .matchId(1L)
+                    .pubId(2L)
+                    .latitude(51.7465619)
+                    .longitude(19.4502251)
+                    .numberOfAttendees(20L)
+                    .description("Schalke 04 vs Tottenham pub 2 COTTON CLUB")
                     .build());
         };
     }
@@ -51,17 +117,87 @@ public class MockInjector {
     public CommandLineRunner matchMock(MatchRepository matchRepository) {
         return (args) -> {
             matchRepository.save(Match.builder()
-                    .date(LocalDate.now().plusDays(20))
+                    .date(LocalDate.now().plusDays(9))
                     .awayTeamId(1L)
                     .homeTeamId(2L)
                     .time(LocalTime.now().plusMinutes(4000))
                     .build());
 
             matchRepository.save(Match.builder()
-                    .date(LocalDate.now().plusDays(25))
+                    .date(LocalDate.now().plusDays(9))
+                    .awayTeamId(3L)
+                    .homeTeamId(4L)
+                    .time(LocalTime.now().plusMinutes(4000))
+                    .build());
+
+            matchRepository.save(Match.builder()
+                    .date(LocalDate.now().plusDays(5))
+                    .awayTeamId(2L)
+                    .homeTeamId(1L)
+                    .time(LocalTime.now().plusMinutes(4000))
+                    .build());
+
+            matchRepository.save(Match.builder()
+                    .date(LocalDate.now().plusDays(5))
+                    .awayTeamId(4L)
+                    .homeTeamId(3L)
+                    .time(LocalTime.now().plusMinutes(4000))
+                    .build());
+
+            matchRepository.save(Match.builder()
+                    .date(LocalDate.now().plusDays(11))
+                    .awayTeamId(4L)
+                    .homeTeamId(1L)
+                    .time(LocalTime.now().plusMinutes(4000))
+                    .build());
+
+            matchRepository.save(Match.builder()
+                    .date(LocalDate.now().plusDays(11))
                     .awayTeamId(2L)
                     .homeTeamId(3L)
-                    .time(LocalTime.now().plusMinutes(2400))
+                    .time(LocalTime.now().plusMinutes(4000))
+                    .build());
+
+            matchRepository.save(Match.builder()
+                    .date(LocalDate.now().plusDays(11))
+                    .awayTeamId(1L)
+                    .homeTeamId(4L)
+                    .time(LocalTime.now().plusMinutes(4000))
+                    .build());
+
+            matchRepository.save(Match.builder()
+                    .date(LocalDate.now().plusDays(11))
+                    .awayTeamId(3L)
+                    .homeTeamId(2L)
+                    .time(LocalTime.now().plusMinutes(4000))
+                    .build());
+
+            matchRepository.save(Match.builder()
+                    .date(LocalDate.now().plusDays(16))
+                    .awayTeamId(1L)
+                    .homeTeamId(3L)
+                    .time(LocalTime.now().plusMinutes(4000))
+                    .build());
+
+            matchRepository.save(Match.builder()
+                    .date(LocalDate.now().plusDays(16))
+                    .awayTeamId(4L)
+                    .homeTeamId(2L)
+                    .time(LocalTime.now().plusMinutes(4000))
+                    .build());
+
+            matchRepository.save(Match.builder()
+                    .date(LocalDate.now().plusDays(20))
+                    .awayTeamId(3L)
+                    .homeTeamId(1L)
+                    .time(LocalTime.now().plusMinutes(4000))
+                    .build());
+
+            matchRepository.save(Match.builder()
+                    .date(LocalDate.now().plusDays(16))
+                    .awayTeamId(2L)
+                    .homeTeamId(4L)
+                    .time(LocalTime.now().plusMinutes(4000))
                     .build());
         };
     }
@@ -70,19 +206,45 @@ public class MockInjector {
     public CommandLineRunner teamMock(TeamRepository teamRepository) {
         return (args) -> {
             teamRepository.save(Team.builder()
-                    .name("Raków Częstochowa")
-                    .imgUrl("https://e.kiwip.pl/rakow.png")
-                    .build());
-
-            teamRepository.save(Team.builder()
-                    .name("Lech Poznań")
-                    .imgUrl("https://upload.wikimedia.org/wikipedia/ro/thumb/0/0e/Lech_Pozna%C5%84.svg/194px-Lech_Pozna%C5%84.svg.png")
-                    .build());
-
-            teamRepository.save(Team.builder()
                     .name("Real Madryt")
                     .imgUrl("https://vignette.wikia.nocookie.net/nonsensopedia/images/7/7e/Real_madrid_logo_copy_by_PiotroV.png/revision/latest?cb=20090724183238")
                     .build());
+
+            teamRepository.save(Team.builder()
+                    .name("Liverpool F.C.")
+                    .imgUrl("https://upload.wikimedia.org/wikipedia/en/thumb/0/0c/Liverpool_FC.svg/255px-Liverpool_FC.svg.png")
+                    .build());
+
+            teamRepository.save(Team.builder()
+                    .name("Tottenham Hotspur F.C.")
+                    .imgUrl("https://ssl.gstatic.com/onebox/media/sports/logos/k3Q_mKE98Dnohrcea0JFgQ_96x96.png")
+                    .build());
+
+            teamRepository.save(Team.builder()
+                    .name("FC Schalke 04")
+                    .imgUrl("https://ssl.gstatic.com/onebox/media/sports/logos/3CjFxnIvrHJ1-pJq6B5ZQw_96x96.png")
+                    .build());
+
+            teamRepository.save(Team.builder()
+                    .name("Termalica Bruk-Bet Nieciecza")
+                    .imgUrl("http://www.termalica.brukbet.com/nowa/assets/img/herb.png")
+                    .build());
+
+            teamRepository.save(Team.builder()
+                    .name("SSC Napoli")
+                    .imgUrl("https://ssl.gstatic.com/onebox/media/sports/logos/PWRLYBJqlGrAAsKkUN6eng_96x96.png")
+                    .build());
+
+            teamRepository.save(Team.builder()
+                    .name("Paris Saint-Germain F.C.")
+                    .imgUrl("https://ssl.gstatic.com/onebox/media/sports/logos/mcpMspef1hwHwi9qrfp4YQ_96x96.png")
+                    .build());
+
+            teamRepository.save(Team.builder()
+                    .name("Juventus F.C.")
+                    .imgUrl("https://ssl.gstatic.com/onebox/media/sports/logos/Lv6xmBlUIpN3GAFhtf6nqQ_96x96.png")
+                    .build());
+
         };
     }
 
